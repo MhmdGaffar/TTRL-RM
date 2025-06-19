8f1e3827bdbee8ad66e579c5862038368bc869dd
Muhammad "Gaffar" Omer <mhmdgaffar98@gmail.com>
2025-06-19 18:32:57 +0200

    Update preprocess.py

--- verl/data/preprocess.py
+++ verl/data/preprocess.py
@@ -34,7 +34,7 @@ def make_map_fn(split, source=None):
 
 if __name__ == '__main__':
 
-    data_source = 'AIME-TTT'
+    data_source = 'data/AIME-TTT'
 
     train_dataset = datasets.load_dataset("json", data_files=os.path.join(data_source, 'train.json'), split='train')
     test_dataset = datasets.load_dataset("json", data_files=os.path.join(data_source, 'test.json'), split='train')
