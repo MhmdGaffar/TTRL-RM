7e0e676cf69604d6ff9500100dd69077419e4623
Ahmed-5 <ahmedelhassen11@gmail.com>
2025-07-09 13:04:43 +0200

    debug

--- verl/verl/trainer/ppo/ray_trainer.py
+++ verl/verl/trainer/ppo/ray_trainer.py
@@ -624,7 +624,9 @@ class RayPPOTrainer:
             test_batch = test_batch.union(test_output_gen_batch)
 
             # evaluate using reward_function
+            print("Validation reward function start", self.val_reward_fn.__name__)
             result = self.val_reward_fn(test_batch, return_dict=True)
+            print("Validation reward function end", self.val_reward_fn.__name__)
             reward_tensor = result["reward_tensor"]
             scores = reward_tensor.sum(-1).cpu().tolist()
             sample_scores.extend(scores)
